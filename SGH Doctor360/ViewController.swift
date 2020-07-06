8c25f158299722504027aabb18fbfe6554f496dc
berothomas433 <eng.ebramthomas@gmail.com>
2020-07-06 11:25:41 +0200

    Initial Commit

---
+++ SGH Doctor360/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SGH Doctor360
+//
+//  Created by ebram thomas on 7/6/20.
+//  Copyright © 2020 ebram thomas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
